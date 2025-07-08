e150166f721f1599f8127ae6853cc238bec2a13b
mah2912 <mahtianandrianina@gmail.com>
2025-07-08 07:02:47 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCkwwwboM3lGW_284ZN5RZWJmmSL7JwkJU",
+  authDomain: "mah-hotel.firebaseapp.com",
+  projectId: "mah-hotel",
+  storageBucket: "mah-hotel.appspot.com",
+  messagingSenderId: "389877777937",
+  appId: "1:389877777937:web:1c631c128857be29ab53b0"
+};
+
+// Initialisation
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
